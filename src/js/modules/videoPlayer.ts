a1b416b836e608c00c888dc96c798f78df817a7e
Oleksandr Tereshchuk <tereshchuk.oleksandr@itsolution.expert>
2023-09-17 00:45:30 +0300

    fix videoPlayer script

--- src/js/modules/videoPlayer.ts
+++ src/js/modules/videoPlayer.ts
@@ -1,8 +1,8 @@
 const videoPlayerMockup = document.querySelector('.videoPlayerMockup')!;
-const videoPlayer: HTMLVideoElement = document.getElementById('videoPlayer')!;
+const videoPlayer: any = document.getElementById('videoPlayer')!;
 
 videoPlayerMockup.addEventListener('click', function () {
-	(this as HTMLDivElement).classList.add('hidden');
+	videoPlayerMockup.classList.add('hidden');
 
 	const videoPath: string | undefined = videoPlayer?.dataset.src;
 	videoPlayer?.classList.remove('hidden');
