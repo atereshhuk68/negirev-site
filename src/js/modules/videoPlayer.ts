8ad50291e319228d0296e0102d840b548afc6ebb
Oleksandr Tereshchuk <tereshchuk.oleksandr@itsolution.expert>
2023-09-17 15:18:49 +0300

    fix video path

--- src/js/modules/videoPlayer.ts
+++ src/js/modules/videoPlayer.ts
@@ -1,14 +1,12 @@
+import video from '../../assets/video/video.mp4?url';
+
 const videoPlayerMockup = document.querySelector('.videoPlayerMockup')!;
 const videoPlayer: any = document.getElementById('videoPlayer')!;
 
 videoPlayerMockup.addEventListener('click', function () {
 	videoPlayerMockup.classList.add('hidden');
-
-	const videoPath: string | undefined = videoPlayer?.dataset.src;
 	videoPlayer?.classList.remove('hidden');
-	if (videoPath) {
-		videoPlayer?.setAttribute('src', videoPath);
-		videoPlayer?.removeAttribute('data-src');
-		videoPlayer?.play();
-	}
+	videoPlayer?.setAttribute('src', video);
+	videoPlayer?.removeAttribute('data-src');
+	videoPlayer?.play();
 });
