b91438a135bde0ddd6924d2562d14281cbbb4367
Oleksandr Tereshchuk <tereshchuk.oleksandr@itsolution.expert>
2023-09-18 20:41:45 +0300

    change base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/negirev-site/",
   resolve: {
     alias: {
       "@fonts": resolve(__dirname, "src/assets/fonts/"),
