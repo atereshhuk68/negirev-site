3e194c80a2b1712fb5d5afbf3dfed6c860ecb37e
Oleksandr Tereshchuk <tereshchuk.oleksandr@itsolution.expert>
2023-09-18 20:28:18 +0300

    content fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		screens: {
-			sm: '576px',
-			md: '768px',
-			lg: '992px',
-			xl: '1200px',
-		},
-		container: {
-			center: true,
-		},
-		fontFamily: {
-			serif: ['Noto Serif', 'serif'],
-			sans: ['Inter', 'sans-serif'],
-			body: ['Noto Serif', 'serif'],
-		},
-	},
-	plugins: [],
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      serif: ["Noto Serif", "serif"],
+      sans: ["Inter", "sans-serif"],
+      body: ["Noto Serif", "serif"],
+      arial: ["Arial", "sans-serif"],
+    },
+  },
+  plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,22 @@
 // vite.config.js
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import htmlMinifier from 'vite-plugin-html-minifier'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: '/negirev-site/',
-	resolve: {
-		alias: {
-			'@fonts': resolve(__dirname, 'src/assets/fonts/'),
-			'@modules': resolve(__dirname, 'src/js/modules/'),
-		},
-	},
-	build: {
-		rollupOptions: {
-			input: {
-				home: resolve(__dirname, 'index.html'),
-				about: resolve(__dirname, 'about.html'),
-				consultation: resolve(__dirname, 'consultation.html'),
-			},
-		},
-	},
-	plugins: [
-    htmlMinifier({
-      minify: true,
-    }),
-  ],
+  base: "/",
+  resolve: {
+    alias: {
+      "@fonts": resolve(__dirname, "src/assets/fonts/"),
+      "@modules": resolve(__dirname, "src/js/modules/"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        consultation: resolve(__dirname, "consultation.html"),
+      },
+    },
+  },
 });
