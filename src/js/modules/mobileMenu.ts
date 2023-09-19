45b0aafea5e868dda48a46ca83af7b51b7eb0cca
Oleksandr Tereshchuk <tereshchuk.oleksandr@itsolution.expert>
2023-09-19 19:40:00 +0300

    mobile version fixs

--- src/js/modules/mobileMenu.ts
+++ src/js/modules/mobileMenu.ts
@@ -1,59 +1,59 @@
-const headerNavContainer = document.querySelector('.headerNavContainer')!;
-const headerSocialContainer = document.querySelector('.headerSocialContainer')!;
+const headerNavContainer = document.querySelector(".headerNavContainer")!;
+const headerSocialContainer = document.querySelector(".headerSocialContainer")!;
 
-const headerNav = document.getElementById('headerNav')!;
-const headerSocial = document.getElementById('headerSocial')!;
+const headerNav = document.getElementById("headerNav")!;
+const headerSocial = document.getElementById("headerSocial")!;
 
-const headerMobileMenu = document.getElementById('headerMobileMenu')!;
+const headerMobileMenu = document.getElementById("headerMobileMenu")!;
 
-const headerBurger = document.getElementById('headerBurger')!;
+const headerBurger = document.getElementById("headerBurger")!;
 
-headerBurger.addEventListener('click', () => {});
+headerBurger.addEventListener("click", () => {});
 
 function openMobileMenu(): void {
-	headerMobileMenu.classList.remove('hidden');
-	headerMobileMenu.classList.add('flex');
+  headerMobileMenu.classList.remove("hidden");
+  headerMobileMenu.classList.add("flex");
 
-	window.scrollTo(0, 0);
-	document.body.classList.add('overflow-hidden');
+  window.scrollTo(0, 0);
+  // document.body.classList.add('overflow-hidden');
 
-	headerBurger.classList.add('is-active');
+  headerBurger.classList.add("is-active");
 }
 
 function closeMobileMenu(): void {
-	headerMobileMenu.classList.add('hidden');
-	headerMobileMenu.classList.remove('flex');
+  headerMobileMenu.classList.add("hidden");
+  headerMobileMenu.classList.remove("flex");
 
-	document.body.classList.remove('overflow-hidden');
+  // document.body.classList.remove('overflow-hidden');
 
-	headerBurger.classList.remove('is-active');
+  headerBurger.classList.remove("is-active");
 }
 
 function toggleMobileMenu(): void {
-	if (headerMobileMenu.classList.contains('hidden')) {
-		openMobileMenu();
-	} else {
-		closeMobileMenu();
-	}
+  if (headerMobileMenu.classList.contains("hidden")) {
+    openMobileMenu();
+  } else {
+    closeMobileMenu();
+  }
 }
 
 function moveHeaderElements(): void {
-	if (window.matchMedia('(max-width: 991.98px)').matches) {
-		headerMobileMenu.insertAdjacentElement('beforeend', headerNav);
-		headerMobileMenu.insertAdjacentElement('beforeend', headerSocial);
-	} else {
-		headerNavContainer.insertAdjacentElement('beforeend', headerNav);
-		headerSocialContainer.insertAdjacentElement('beforeend', headerSocial);
-	}
+  if (window.matchMedia("(max-width: 991.98px)").matches) {
+    headerMobileMenu.insertAdjacentElement("beforeend", headerNav);
+    headerMobileMenu.insertAdjacentElement("beforeend", headerSocial);
+  } else {
+    headerNavContainer.insertAdjacentElement("beforeend", headerNav);
+    headerSocialContainer.insertAdjacentElement("beforeend", headerSocial);
+  }
 }
 
 function initListeners(): void {
-	headerNavContainer.classList.remove('hidden');
-	headerSocialContainer.classList.remove('hidden');
+  headerNavContainer.classList.remove("hidden");
+  headerSocialContainer.classList.remove("hidden");
 
-	window.addEventListener('resize', moveHeaderElements);
-	moveHeaderElements();
-	headerBurger.addEventListener('click', toggleMobileMenu);
+  window.addEventListener("resize", moveHeaderElements);
+  moveHeaderElements();
+  headerBurger.addEventListener("click", toggleMobileMenu);
 }
 
 initListeners();
