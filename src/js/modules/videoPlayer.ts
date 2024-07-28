e792ff1be40297583e4ce05fab82db994157f3a3
Oleksandr Tereshchuk <otereshhuk@proton.me>
2024-07-28 22:51:13 +0300

    perf(images): add avif format for faster loading and optimize image handling

--- src/js/modules/videoPlayer.ts
+++ src/js/modules/videoPlayer.ts
@@ -1,12 +1,12 @@
-import video from '../../assets/video/video.mp4?url';
+import video from "/video/video.mp4?url";
 
-const videoPlayerMockup = document.querySelector('.videoPlayerMockup')!;
-const videoPlayer: any = document.getElementById('videoPlayer')!;
+const videoPlayerMockup = document.querySelector(".videoPlayerMockup")!;
+const videoPlayer: any = document.getElementById("videoPlayer")!;
 
-videoPlayerMockup.addEventListener('click', function () {
-	videoPlayerMockup.classList.add('hidden');
-	videoPlayer?.classList.remove('hidden');
-	videoPlayer?.setAttribute('src', video);
-	videoPlayer?.removeAttribute('data-src');
-	videoPlayer?.play();
+videoPlayerMockup.addEventListener("click", function () {
+  videoPlayerMockup.classList.add("hidden");
+  videoPlayer?.classList.remove("hidden");
+  videoPlayer?.setAttribute("src", video);
+  videoPlayer?.removeAttribute("data-src");
+  videoPlayer?.play();
 });
